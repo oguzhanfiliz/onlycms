176551797171fd57e32ee563fca3abd9591f1ca3
Oğuzhan Filiz <oguzhan.filiz@ogr.dpu.edu.tr>
2020-08-15 13:54:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -66,8 +66,8 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
+		error_reporting(0);
+		ini_set('display_errors', 0);
 	break;
 
 	case 'testing':
